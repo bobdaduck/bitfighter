3be7c054c61d36215d8acceb0c7743634b1f70cc
bobdaduck <jonathanrhansen@yahoo.com>
2020-05-20 22:27:57 -0600

    Fix copy paste issues

--- zap/GameSettings.h
+++ zap/GameSettings.h
@@ -23,62 +23,62 @@ using namespace TNL;
 namespace CmdLineParams
 {
 
-   enum ParamId {
-      LOGIN_NAME,
-      LOGIN_PASSWORD,
-      WINDOW_MODE,
-      FULLSCREEN_MODE,
-      FULLSCREEN_STRETCH,
-      WINDOW_POS,
-      WINDOW_WIDTH,
-      USE_STICK,
-      NO_MUSIC,
-      MASTER_ADDRESS,
+enum ParamId {
+   LOGIN_NAME,
+   LOGIN_PASSWORD,
+   WINDOW_MODE,
+   FULLSCREEN_MODE,
+   FULLSCREEN_STRETCH,
+   WINDOW_POS,
+   WINDOW_WIDTH,
+   USE_STICK,
+   NO_MUSIC,
+   MASTER_ADDRESS,
 
-      DEDICATED,
-      HOST_ON_DEDICATED,
-      SERVER_PASSWORD,
-      OWNER_PASSWORD,
-      ADMIN_PASSWORD,
-      NO_ADMIN_PASSWORD,
-      LEVEL_CHANGE_PASSWORD,
-      NO_LEVEL_CHANGE_PASSWORD,
-      HOST_NAME,
-      HOST_DESCRIPTION,
-      MAX_PLAYERS_PARAM,
-      HOST_ADDRESS,
+   DEDICATED,
+   HOST_ON_DEDICATED,
+   SERVER_PASSWORD,
+   OWNER_PASSWORD,
+   ADMIN_PASSWORD,
+   NO_ADMIN_PASSWORD,
+   LEVEL_CHANGE_PASSWORD,
+   NO_LEVEL_CHANGE_PASSWORD,
+   HOST_NAME,
+   HOST_DESCRIPTION,
+   MAX_PLAYERS_PARAM,
+   HOST_ADDRESS,
 
-      LEVEL_LIST,
-      USE_FILE,
+   LEVEL_LIST,
+   USE_FILE,
 
-      ROOT_DATA_DIR,
-      PLUGIN_DIR,
-      LEVEL_DIR,
-      PLAYLIST_FILE_DIR,
-      INI_DIR,
-      LOG_DIR,
-      SCRIPTS_DIR,
-      ROBOT_DIR,
-      SCREENSHOT_DIR,
-      SFX_DIR,
-      MUSIC_DIR,
-      FONTS_DIR,
-      RECORD_DIR,
+   ROOT_DATA_DIR,
+   PLUGIN_DIR,
+   LEVEL_DIR,
+   PLAYLIST_FILE_DIR,
+   INI_DIR,
+   LOG_DIR,
+   SCRIPTS_DIR,
+   ROBOT_DIR,
+   SCREENSHOT_DIR,
+   SFX_DIR,
+   MUSIC_DIR,
+   FONTS_DIR,
+   RECORD_DIR,
 
-      SIMULATED_LOSS,
-      SIMULATED_LAG,
-      SIMULATED_STUTTER,
-      FORCE_UPDATE,
+   SIMULATED_LOSS,
+   SIMULATED_LAG,
+   SIMULATED_STUTTER,
+   FORCE_UPDATE,
 
-      SEND_RESOURCE,
-      GET_RESOURCE,
-      SHOW_RULES,
-      SHOW_LUA_CLASSES,
-      HELP,
-      VERSION,
+   SEND_RESOURCE,
+   GET_RESOURCE,
+   SHOW_RULES,
+   SHOW_LUA_CLASSES,
+   HELP,
+   VERSION,
 
-      PARAM_COUNT
-   };
+   PARAM_COUNT
+};
 
 };
 
@@ -88,227 +88,227 @@ using namespace CmdLineParams;
 namespace Zap
 {
 
-   class BanList;
-   struct PluginBinding;
+class BanList;
+struct PluginBinding;
 
 
-   enum SettingSource {
-      INI,
-      CMD_LINE,
-      DEFAULT
-   };
+enum SettingSource {
+   INI,
+   CMD_LINE,
+   DEFAULT
+};
 
 
-   class GameSettings
-   {
-      typedef map<string, UserSettings> UserSettingsMap;
+class GameSettings
+{
+   typedef map<string,UserSettings> UserSettingsMap;
 
-   private:
-      // Some items will be passthroughs to the underlying INI object; however, if a value can differ from the INI setting 
-      // (such as when it can be overridden from the cmd line, or is set remotely), then we'll need to store the working value locally.
+private:
+   // Some items will be passthroughs to the underlying INI object; however, if a value can differ from the INI setting 
+   // (such as when it can be overridden from the cmd line, or is set remotely), then we'll need to store the working value locally.
 
-      string mHostName;                   // Server name used when hosting a game (default set in config.h, set in INI or on cmd line)
-      string mHostDescr;                  // Brief description of host
+   string mHostName;                   // Server name used when hosting a game (default set in config.h, set in INI or on cmd line)
+   string mHostDescr;                  // Brief description of host
 
-      string mPlayerName, mPlayerPassword;   // Resolved name/password, either from INI for cmdLine or login screen
-      bool mPlayerNameSpecifiedOnCmdLine;
+   string mPlayerName, mPlayerPassword;   // Resolved name/password, either from INI for cmdLine or login screen
+   bool mPlayerNameSpecifiedOnCmdLine;
 
-      // Various passwords
-      string mServerPassword;
-      string mOwnerPassword;
-      string mAdminPassword;
-      string mLevelChangePassword;
+   // Various passwords
+   string mServerPassword;
+   string mOwnerPassword;
+   string mAdminPassword;
+   string mLevelChangePassword;
 
-      Vector<string> mLevelSkipList;      // Levels we'll never load, to create a pseudo delete function for remote server mgt  <=== does this ever get loaded???
-      static FolderManager* mFolderManager;
-      InputCodeManager mInputCodeManager;
+   Vector<string> mLevelSkipList;      // Levels we'll never load, to create a pseudo delete function for remote server mgt  <=== does this ever get loaded???
+   static FolderManager *mFolderManager;
+   InputCodeManager mInputCodeManager;
 
-      BanList* mBanList;                  // Our ban list
+   BanList *mBanList;                  // Our ban list
 
-      //CmdLineSettings mCmdLineSettings;
-      IniSettings mIniSettings;
+   //CmdLineSettings mCmdLineSettings;
+   IniSettings mIniSettings;
 
-      // Store params read from the cmd line
-      Vector<string> mCmdLineParams[CmdLineParams::PARAM_COUNT];
+   // Store params read from the cmd line
+   Vector<string> mCmdLineParams[CmdLineParams::PARAM_COUNT];
 
-      // User settings storage
-      UserSettingsMap mUserSettings;
+   // User settings storage
+   UserSettingsMap mUserSettings;
 
-      Vector<string> mMasterServerList;
-      bool mMasterServerSpecifiedOnCmdLine;
+   Vector<string> mMasterServerList;
+   bool mMasterServerSpecifiedOnCmdLine;
 
-      // Helper functions:
-      // This first lot return the first value following the cmd line parameter cast to various types
-      string getString(ParamId paramId) const;
-      U32 getU32(ParamId paramId) const;
-      F32 getF32(ParamId paramId) const;
+   // Helper functions:
+   // This first lot return the first value following the cmd line parameter cast to various types
+   string getString(ParamId paramId) const;
+   U32 getU32(ParamId paramId) const;
+   F32 getF32(ParamId paramId) const;
 
-      DisplayMode resolveCmdLineSpecifiedDisplayMode();  // Tries to figure out what display mode was specified on the cmd line, if any
+   DisplayMode resolveCmdLineSpecifiedDisplayMode();  // Tries to figure out what display mode was specified on the cmd line, if any
+      
+   Vector<LoadoutTracker> mLoadoutPresets;
 
-      Vector<LoadoutTracker> mLoadoutPresets;
+   Vector<string> mConfigurationErrors;
+   Vector<string> getLevelList(const string &levelDir, bool ignoreCmdLine);    
+   Vector<string> getPlaylist();       
 
-      Vector<string> mConfigurationErrors;
-      Vector<string> getLevelList(const string& levelDir, bool ignoreCmdLine);
-      Vector<string> getPlaylist();
+public:
+   GameSettings();            // Constructor
+   virtual ~GameSettings();   // Destructor
 
-   public:
-      GameSettings();            // Constructor
-      virtual ~GameSettings();   // Destructor
+   static CIniFile iniFile;
+   static CIniFile userPrefs;
 
-      static CIniFile iniFile;
-      static CIniFile userPrefs;
+   static const S32 LoadoutPresetCount = 3;     // How many presets do we save?
 
-      static const S32 LoadoutPresetCount = 3;     // How many presets do we save?
+   static const U16 DEFAULT_GAME_PORT = 28000;
 
-      static const U16 DEFAULT_GAME_PORT = 28000;
 
+   void readCmdLineParams(const Vector<string> &argv);
+   void resolveDirs();
 
-      void readCmdLineParams(const Vector<string>& argv);
-      void resolveDirs();
+   string getHostName();
+   void setHostName(const string &hostName, bool updateINI);
 
-      string getHostName();
-      void setHostName(const string& hostName, bool updateINI);
+   string getHostDescr();
+   void setHostDescr(const string &hostDescr, bool updateINI);
 
-      string getHostDescr();
-      void setHostDescr(const string& hostDescr, bool updateINI);
+   string getWelcomeMessage() const;
+   void setWelcomeMessage(const string &welcomeMessage);
 
-      string getWelcomeMessage() const;
-      void setWelcomeMessage(const string& welcomeMessage);
+   string getGlobalLevelgenScript() const;
+   void setGlobalLevelgenScript(const string& GlobalLevelgenScript);
 
-      string getGlobalLevelgenScript() const;
-      void setGlobalLevelgenScript(const string& GlobalLevelgenScript);
+   string getServerPassword();
+   void setServerPassword(const string &ServerPassword, bool updateINI);
 
-      string getServerPassword();
-      void setServerPassword(const string& ServerPassword, bool updateINI);
+   string getOwnerPassword();
+   void setOwnerPassword(const string &OwnerPassword, bool updateINI);
 
-      string getOwnerPassword();
-      void setOwnerPassword(const string& OwnerPassword, bool updateINI);
+   string getAdminPassword();
+   void setAdminPassword(const string &AdminPassword, bool updateINI);
 
-      string getAdminPassword();
-      void setAdminPassword(const string& AdminPassword, bool updateINI);
+   string getLevelChangePassword();
+   void setLevelChangePassword(const string &LevelChangePassword, bool updateINI);
 
-      string getLevelChangePassword();
-      void setLevelChangePassword(const string& LevelChangePassword, bool updateINI);
+   InputCodeManager *getInputCodeManager(); 
 
-      InputCodeManager* getInputCodeManager();
+   Vector<string> *getLevelSkipList();
+   Vector<string> *getSpecifiedLevels();
 
-      Vector<string>* getLevelSkipList();
-      Vector<string>* getSpecifiedLevels();
+   void setLoginCredentials(const string &name, const string &password, bool savePassword);
 
-      void setLoginCredentials(const string& name, const string& password, bool savePassword);
 
+   bool getSpecified(ParamId paramId);                      // Returns true if parameter was present, false if not
 
-      bool getSpecified(ParamId paramId);                      // Returns true if parameter was present, false if not
+   // Variations on generating a list of levels
+   Vector<string> getLevelList();                            // Generic, grab a list of levels based on current settings
+   Vector<string> getLevelList(const string &levelFolder);   // Grab a list of levels from the specified level folder; ignore anything in the INI
 
-      // Variations on generating a list of levels
-      Vector<string> getLevelList();                            // Generic, grab a list of levels based on current settings
-      Vector<string> getLevelList(const string& levelFolder);   // Grab a list of levels from the specified level folder; ignore anything in the INI
+public:
+   static S32 UseControllerIndex;  // Which SDL2 controller index are we using
 
-   public:
-      static S32 UseControllerIndex;  // Which SDL2 controller index are we using
+   Vector<string> *getMasterServerList();
+   void saveMasterAddressListInIniUnlessItCameFromCmdLine();
+   
+   static FolderManager *getFolderManager();
+   FolderManager getCmdLineFolderManager();    // Return a FolderManager struct populated with settings specified on cmd line
 
-      Vector<string>* getMasterServerList();
-      void saveMasterAddressListInIniUnlessItCameFromCmdLine();
+   BanList *getBanList();
 
-      static FolderManager* getFolderManager();
-      FolderManager getCmdLineFolderManager();    // Return a FolderManager struct populated with settings specified on cmd line
+   string getHostAddress();
+   U32 getMaxPlayers();
 
-      BanList* getBanList();
+   void save();
 
-      string getHostAddress();
-      U32 getMaxPlayers();
+   IniSettings *getIniSettings();
 
-      void save();
+   void runCmdLineDirectives();
 
-      IniSettings* getIniSettings();
+   bool shouldShowNameEntryScreenOnStartup();
 
-      void runCmdLineDirectives();
+   const Color *getWallFillColor() const;
+   const Color *getWallOutlineColor() const;
 
-      bool shouldShowNameEntryScreenOnStartup();
+   void setQueryServerSortColumn(S32 column, bool ascending);
+   S32  getQueryServerSortColumn();   
+   bool getQueryServerSortAscending();
 
-      const Color* getWallFillColor() const;
-      const Color* getWallOutlineColor() const;
 
-      void setQueryServerSortColumn(S32 column, bool ascending);
-      S32  getQueryServerSortColumn();
-      bool getQueryServerSortAscending();
+   // Accessor methods
+   U32 getSimulatedStutter();
+   F32 getSimulatedLoss();
+   U32 getSimulatedLag();
 
+   string getDefaultName();
 
-      // Accessor methods
-      U32 getSimulatedStutter();
-      F32 getSimulatedLoss();
-      U32 getSimulatedLag();
+   bool getForceUpdate();
 
-      string getDefaultName();
+   string getPlayerName();
 
-      bool getForceUpdate();
+   void updatePlayerName(const string &name);
 
-      string getPlayerName();
+   void setAutologin(bool autologin);
 
-      void updatePlayerName(const string& name);
+   string getPlayerPassword();
 
-      void setAutologin(bool autologin);
+   bool isDedicatedServer();
 
-      string getPlayerPassword();
+   string getLevelDir(SettingSource source) const;
+   string getPlaylistFile();
+   bool isUsingPlaylist();
+   string getlevelLoc();
 
-      bool isDedicatedServer();
+   LevelSource *chooseLevelSource(Game *game); // determines what levelsource you want to use
 
-      string getLevelDir(SettingSource source) const;
-      string getPlaylistFile();
-      bool isUsingPlaylist();
-      string getlevelLoc();
+   LoadoutTracker getLoadoutPreset(S32 index);
+   void setLoadoutPreset(const LoadoutTracker *preset, S32 index);
 
-      LevelSource* chooseLevelSource(Game* game); // determines what levelsource you want to use
+   void addConfigurationError(const string &errorMessage);
+   Vector<string> getConfigurationErrors();
 
-      LoadoutTracker getLoadoutPreset(S32 index);
-      void setLoadoutPreset(const LoadoutTracker* preset, S32 index);
+   // Other methods
+   void saveLevelChangePassword(const string &serverName, const string &password);
+   void saveAdminPassword(const string &serverName, const string &password);
+   void saveOwnerPassword(const string &serverName, const string &password);
 
-      void addConfigurationError(const string& errorMessage);
-      Vector<string> getConfigurationErrors();
+   void forgetLevelChangePassword(const string &serverName);
+   void forgetAdminPassword(const string &serverName);
+   void forgetOwnerPassword(const string &serverName);
 
-      // Other methods
-      void saveLevelChangePassword(const string& serverName, const string& password);
-      void saveAdminPassword(const string& serverName, const string& password);
-      void saveOwnerPassword(const string& serverName, const string& password);
+   void onFinishedLoading();     // Should be run after INI and cmd line params have been read
 
-      void forgetLevelChangePassword(const string& serverName);
-      void forgetAdminPassword(const string& serverName);
-      void forgetOwnerPassword(const string& serverName);
+   static void getRes(GameSettings *settings, const Vector<string> &words);
+   static void sendRes(GameSettings *settings, const Vector<string> &words);
+   static void showRules(GameSettings *settings, const Vector<string> &words);
+   static void showHelp(GameSettings *settings, const Vector<string> &words);
+   static void showVersion(GameSettings *settings, const Vector<string> &words);
 
-      void onFinishedLoading();     // Should be run after INI and cmd line params have been read
+   static map<S32,string> DetectedControllerList;   // List of joysticks we found attached to this machine
 
-      static void getRes(GameSettings* settings, const Vector<string>& words);
-      static void sendRes(GameSettings* settings, const Vector<string>& words);
-      static void showRules(GameSettings* settings, const Vector<string>& words);
-      static void showHelp(GameSettings* settings, const Vector<string>& words);
-      static void showVersion(GameSettings* settings, const Vector<string>& words);
+   // Dealing with saved passwords for servers
+   static void saveServerPassword(const string &serverName, const string &password);
+   static string getServerPassword(const string &serverName);
+   static void deleteServerPassword(const string &serverName);
 
-      static map<S32, string> DetectedControllerList;   // List of joysticks we found attached to this machine
+   bool isLevelOnSkipList(const string &filename) const;
+   void addLevelToSkipList(const string &filename);
+   void removeLevelFromSkipList(const string &filename);
+   void saveSkipList() const;
 
-      // Dealing with saved passwords for servers
-      static void saveServerPassword(const string& serverName, const string& password);
-      static string getServerPassword(const string& serverName);
-      static void deleteServerPassword(const string& serverName);
+   // InputCode related
+   InputMode getInputMode();
 
-      bool isLevelOnSkipList(const string& filename) const;
-      void addLevelToSkipList(const string& filename);
-      void removeLevelFromSkipList(const string& filename);
-      void saveSkipList() const;
+   // In-game help messages
+   void setShowingInGameHelp(bool show);
+   bool getShowingInGameHelp();
 
-      // InputCode related
-      InputMode getInputMode();
+   // User settings
+   const UserSettings *addUserSettings(const UserSettings &userSettings);     // Returns pointer to inserted item
+   const UserSettings *getUserSettings(const string &name);
+};
 
-      // In-game help messages
-      void setShowingInGameHelp(bool show);
-      bool getShowingInGameHelp();
 
-      // User settings
-      const UserSettings* addUserSettings(const UserSettings& userSettings);     // Returns pointer to inserted item
-      const UserSettings* getUserSettings(const string& name);
-   };
-
-
-   typedef shared_ptr<GameSettings> GameSettingsPtr;
+typedef shared_ptr<GameSettings> GameSettingsPtr;
 
 
 };
